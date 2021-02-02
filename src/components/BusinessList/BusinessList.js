7b35a2cea8c7367860d16157b99db67561aaae98
kanaetochi okiyi <65520324+okiyitooo@users.noreply.github.com>
2021-02-02 13:50:59 -0500

    used react to make appearance, just finished reducing duplicate code

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './BusinessList.css'
+import Business from '../Business/Business'
+
+export default class BusinessList extends React.Component {
+    render() {
+        return ( 
+            <div className="BusinessList">
+            {this.props.businesses.map(business=><Business business={business}/>)}
+            </div>
+               )
+    }
+}
